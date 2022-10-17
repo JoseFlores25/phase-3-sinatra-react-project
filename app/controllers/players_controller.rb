9dc7a503b99f925fefafc72e08aa66d4705bddf8
JoseFlores25 <74205482+JoseFlores25@users.noreply.github.com>
2022-10-17 01:17:27 -0400

    Add update and delete

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,8 +9,22 @@ class PlayersController < ApplicationController
       player.to_json
   end
 
-  delete '/players/:id' do
+  delete '/players/:id/team/:team_id' do
       player = Player.find_by(id: params[:id])
       player.destroy
+      teams = Team.find_by(id: params[:team_id])
+      teams.to_json(include: :players)
   end
+
+  patch '/players/:id/team/:team_id' do
+    player = Player.find_by(id: params[:id])
+
+    request.body.rewind
+    request_payload = JSON.parse request.body.read
+
+    player.update(request_payload)
+    teams = Team.find_by(id: params[:team_id])
+      teams.to_json(include: :players)
+end
+
 end
